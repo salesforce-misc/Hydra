2315ce8d48b78caa0e0a6aa5ac7649940b1371df
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2024-01-30 19:01:33 +0530

    Copyright update
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

--- buildSrc/src/main/kotlin/hydra.consumer.sample-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/hydra.consumer.sample-conventions.gradle.kts
@@ -1,3 +1,10 @@
+/***************************************************************************************************
+ *  Copyright (c) 2023, Salesforce, Inc. All rights reserved. SPDX-License-Identifier: 
+ *           Apache License Version 2.0 
+ *  For full license text, see the LICENSE file in the repo root or
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ **************************************************************************************************/
+
 plugins { id("io.ktor.plugin") }
 
 val libs: VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
