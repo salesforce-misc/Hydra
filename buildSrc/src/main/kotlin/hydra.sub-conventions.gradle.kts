2fae67bcb5e821a922d0a7c3b5695be3b607bb49
Gopal S Akshintala <gopala.akshintala@salesforce.com>
2024-02-04 08:35:21 +0530

    Remove nexus publish logic
    Signed-off-by: Gopal S Akshintala <gopala.akshintala@salesforce.com>

--- buildSrc/src/main/kotlin/hydra.sub-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/hydra.sub-conventions.gradle.kts
@@ -26,72 +26,9 @@ java {
   withSourcesJar()
 }
 
-tasks {
-  testlogger.theme = MOCHA
-  withType<Jar> { duplicatesStrategy = DuplicatesStrategy.EXCLUDE }
-  withType<PublishToMavenRepository>().configureEach {
-    doLast {
-      logger.lifecycle(
-        "Successfully uploaded ${publication.groupId}:${publication.artifactId}:${publication.version} to ${repository.name}"
-      )
-    }
-  }
-  withType<PublishToMavenLocal>().configureEach {
-    doLast {
-      logger.lifecycle(
-        "Successfully created ${publication.groupId}:${publication.artifactId}:${publication.version} in MavenLocal"
-      )
-    }
-  }
-}
-
 testing {
   suites {
     val test by getting(JvmTestSuite::class) { useJUnitJupiter("5.10.1") }
   }
 }
 
-publishing {
-  publications.create<MavenPublication>("hydra") {
-    from(components["java"])
-    pom {
-      name.set(artifactId)
-      description.set(project.description)
-      url.set("https://git.soma.salesforce.com/CCSPayments/Hydra")
-      licenses {
-        license {
-          name.set("The Apache License, Version 2.0")
-          url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-        }
-      }
-      developers {
-        developer {
-          id.set("gopala.akshintala@salesforce.com")
-          name.set("Gopal S Akshintala")
-          email.set("gopala.akshintala@salesforce.com")
-        }
-      }
-      scm {
-        connection.set("scm:git:https://git.soma.salesforce.com/ccspayments/Hydra")
-        developerConnection.set("scm:git:git@git.soma.salesforce.com:ccspayments/Hydra.git")
-        url.set("https://git.soma.salesforce.com/ccspayments/hydra")
-      }
-    }
-  }
-  repositories {
-    maven {
-      name = "Nexus"
-      val releasesRepoUrl =
-        uri("https://nexus.soma.salesforce.com/nexus/content/repositories/releases")
-      val snapshotsRepoUrl =
-        uri("https://nexus.soma.salesforce.com/nexus/content/repositories/snapshots")
-      url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-      val nexusUsername: String by project
-      val nexusPassword: String by project
-      credentials {
-        username = nexusUsername
-        password = nexusPassword
-      }
-    }
-  }
-}
